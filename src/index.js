5be5fa41a277aac3107936348ad5cf7ca0565ba0
Hoa Luu <hoamluu@gmail.com>
2018-06-26 10:02:57 -0400

    navbar

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import 'bootswatch/dist/flatly/bootstrap.css';
+import './styles.css';
+// const userAddressInput = document.querySelector('')
+
+// const venue_URL = "https://api.foursquare.com/v2/venues/search?ll=40.7484,-73.9857&client_id=YB2E5I4F5H1INFQNGKCA0LFKQDAKR50JHF0TWXK2NTZTEVN2&client_secret=Q2OGF5LBUOSFI3EVMO2ZP5JTD1QWQB0WJUYRYKOQPVU0EDXN&v=20180323";
+//
+// const geo_URL = "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDPHepsa6b3tEV-A2i_t15HJZIj1uurxbw";
+// const userAddressInput =
+// window.addEventListener('load', event => {
+//   event.preventDefault();
+//
+// })
+//
+// function getAddress(){
+//
+// }
